227783d1a120e59c793708b00dc6e4015fde8f0a
LLL <77712364+elsam0104@users.noreply.github.com>
2022-10-18 20:29:47 +0900

    1018 퀘스트 데이터 베이스 완성, 퀘스트 시스템 생성

--- Assets/Inflearn/Scripts/Quest/QuestDataBase.cs
+++ Assets/Inflearn/Scripts/Quest/QuestDataBase.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 [CreateAssetMenu(menuName ="Quest/QuestDataBase")]
 public class QuestDataBase : ScriptableObject 
 {
@@ -11,4 +15,33 @@ public class QuestDataBase : ScriptableObject
 
     public IReadOnlyList<Quest> Quests => quests;
     public Quest FindQuestBy(string codeName) => quests.FirstOrDefault(x => x.CodeName == codeName);
+#if UNITY_EDITOR
+    [ContextMenu("Find Quests")]
+    private void FindQuests()
+    {
+        FindQuestsBy<Quest>();
+    }
+    [ContextMenu("Find Achivements")]
+    private void FindAchivements()
+    {
+        FindQuestsBy<Achivement>();
+    }
+    public void FindQuestsBy<T>() where T : Quest
+    {
+        quests = new List<Quest>();
+        string[] guids = AssetDatabase.FindAssets($"t:{typeof(T)}");
+        foreach(string guid in guids)
+        {
+            string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+            var quest = AssetDatabase.LoadAssetAtPath<T>(assetPath);
+
+            if(quest.GetType() == typeof(T))
+            {
+                quests.Add(quest);
+            }
+            EditorUtility.SetDirty(this);
+            AssetDatabase.SaveAssets();
+        }
+    }
+#endif
 }
---
+++ Assets/Inflearn/Scripts/Quest/QuestSystem.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class QuestSystem : MonoSingleton<QuestSystem>
+{
+    #region Events
+    public delegate void QuestRegisteredHandler(Quest newQuest);
+    public delegate void QuestCompletedHandler(Quest quest);
+    public delegate void QuestCanceledHandler(Quest quest);
+    #endregion
+
+    private List<Quest> activeQuests = new List<Quest>();
+    private List<Quest> completeQuests = new List<Quest>();
+
+    private List<Achivement> activeAchivements = new List<Achivement>();
+    private List<Achivement> completeAchivements = new List<Achivement>();
+
+    private QuestDataBase questDataBase, achivementDataBase;
+
+    public event QuestRegisteredHandler onQuestRegistered;
+    public event QuestCompletedHandler onQuestCompleted;
+    public event QuestCanceledHandler onQuestCanceled;
+
+    public IReadOnlyList<Quest> ActiveQuests => activeQuests;
+    public IReadOnlyList<Quest> CompleteQuests => completeQuests;
+
+    public IReadOnlyList<Achivement> ActiveAchivements => activeAchivements;
+    public IReadOnlyList<Achivement> CompleteAchivements => completeAchivements;
+
+    private void Awake()
+    {
+        questDataBase = Resources.Load<QuestDataBase>("QuestDataBase");
+        achivementDataBase = Resources.Load<QuestDataBase>("AchivementDataBase");
+    }
+}
