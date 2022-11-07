ff8c44709dfbc04727f705b5d02c23471b45d2ed
YeEun Oh <OhSolDev@gmail.com>
2022-11-08 08:45:27 +0900

    1108 암호화,복호화 모듈 제작
    한승쌤이 알려주심

---
+++ Assets/Scripts/Util/TestCrypto.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCrypto : MonoBehaviour
+{
+    [SerializeField]
+    [TextArea]
+    private string test_str;
+    private string encryptStr;
+    void Start()
+    {
+        encryptStr = Crypto.AESEncrypt128(test_str);
+        Debug.Log("암호화 : "+encryptStr);
+        encryptStr = Crypto.AESDecrypt128(encryptStr);
+        Debug.Log("복호화 : " + encryptStr);
+    }
+
+}
